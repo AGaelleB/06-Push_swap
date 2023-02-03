58bec66564ee63f4a477123473e37df1a38d64c5
Anne-gaelle BONNEFOY <abonnefo@e1r3p8.clusters.42paris.fr>
2023-02-03 12:26:35 +0100

    initialisation de ma libft

---
+++ includes/push_swap.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abonnefo <abonnefo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 09:48:49 by abonnefo          #+#    #+#             */
+/*   Updated: 2023/02/03 09:47:13 by abonnefo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdint.h>
+# include <string.h>
+
+# include "../includes/ft_printf.h"
+# include "../includes/get_next_line.h"
+# include "../includes/libft.h"
+
+/*********************************** push_sawp.c ***********************************/
+
+
+#endif
